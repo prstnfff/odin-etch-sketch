ddd71c4b8c12bd73cd5df0d4600121ae9757b97c
preston <prstnwhite@gmail.com>
2022-10-31 20:52:49 -0700

    added gradual darkening functionality, removed color selector

--- logic.js
+++ logic.js
@@ -1,6 +1,5 @@
 let containerDiv = document.querySelector('.container')
 let resetButton = document.querySelector('.grid-reset')
-let colorInput = document.querySelector('.color-input')
 
 function createGrid(size){
 
@@ -25,15 +24,40 @@ function createGrid(size){
 createGrid(25)
 
 
+const colorGradient = ['#ffffff', '#eeeeee', '#dddddd', '#cccccc', '#bbbbbb', '#aaaaaa', 
+                        '#999999', '#888888', '#777777', '#666666', '#555555', '#444444', 
+                        '#333333', '#222222', '#111111', '#000000']
+
+function colorAdjust(currentColor){
+
+
+    let currentIndex = colorGradient.indexOf(currentColor)
+
+    let newColor = colorGradient[ Math.min( (currentIndex + 1), (colorGradient.length - 1))]
+
+    return(newColor)
+}
+
+/// shamelessly stolen from stackoverflow:  https://stackoverflow.com/questions/1740700/how-to-get-hex-color-value-rather-than-rgb-value
+const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+
 function setUpListeners(){
     document.querySelectorAll('.gridPanel').forEach( gridPiece => {
 
         gridPiece.addEventListener('mouseover', event => {
 
-            event.target.setAttribute('style', `background-color:${colorInput.value}`)
+            let style = getComputedStyle(gridPiece, '') 
+            let currentColor = style.getPropertyValue('background-color') ?? '#fff'
+
+            currentColor = rgb2hex(currentColor)
+
+            newCol = colorAdjust(currentColor)
+            event.target.setAttribute('style', `background-color:${newCol}`)
         })
     })
 }
+
+
 
 resetButton.addEventListener( 'click', event => {
 
